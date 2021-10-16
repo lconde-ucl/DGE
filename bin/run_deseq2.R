877d303c51657bb1342b45bc7a8a6313e290d139
UCL-BLIC <regmr01@ucl.ac.uk>
2021-10-16 18:05:22 +0100

    correct typo

--- bin/run_deseq2.R
+++ bin/run_deseq2.R
@@ -262,8 +262,7 @@ if (default == "no") {
 	de <- rownames(resNorm[resNorm$padj<0.05 & !is.na(resNorm$padj) & abs(resNorm$log2FoldChange) > fc, ])
 	
 	if(length(de) < 5) {
-		message<-paste0("Only ", length(de), " genes with Padj<0.05 and Log2FC>",fc,". Please rerun the pipeline with a lower FC threshold to generate a heatmap"
-		mtext(message, side = 3)
+		message<-paste0("Only ", length(de), " genes with Padj<0.05 and Log2FC>",fc,". Please rerun the pipeline with a lower FC threshold to generate a heatmap")
 		
 		png(file=heatmap_name, width=1500, height=2000, res=200)
 		plot(0, type = 'n', axes = FALSE, ann = FALSE)
@@ -398,8 +397,7 @@ if (default == "no") {
 			de <- rownames(resNorm[resNorm$padj<0.05 & !is.na(resNorm$padj) & abs(resNorm$log2FoldChange) > fc, ])
 			
 			if(length(de) < 5) {
-                		message<-paste0("Only ", length(de), " genes with Padj<0.05 and Log2FC>",fc,". Please rerun the pipeline with a lower FC threshold to generate a heatmap"
-                		mtext(message, side = 3)
+                		message<-paste0("Only ", length(de), " genes with Padj<0.05 and Log2FC>",fc,". Please rerun the pipeline with a lower FC threshold to generate a heatmap")
 
                 		png(file=heatmap_name, width=1500, height=2000, res=200)
                 		plot(0, type = 'n', axes = FALSE, ann = FALSE)
